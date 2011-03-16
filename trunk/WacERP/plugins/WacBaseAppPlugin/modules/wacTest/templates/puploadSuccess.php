37b7a50c6a95c6ac656b2334fade9ffd7108f48c
prince.bi <prince.bi@440668ee-186d-0c73-5080-d64f5aa0851a>
2011-03-16 10:31:10 +0000

    add plupload

--- trunk/WacERP/plugins/WacBaseAppPlugin/modules/wacTest/templates/puploadSuccess.php
+++ trunk/WacERP/plugins/WacBaseAppPlugin/modules/wacTest/templates/puploadSuccess.php
@@ -1,19 +1,17 @@
 <?php
+
 /*
  *  here defines widgets logic of the module
  */
 
-  echo WacWidgetHelper::getInstance()->getWidget(
+echo WacWidgetHelper::getInstance()->getWidget(
         WacModule::getInstance()->getName("wacFileManager"), // be invoked module name
-        WacComponentList::$moduleIndexListWidget, // be invoked widget name
+        WacComponentList::$fileUploadWidget, // be invoked widget name
         array(
             'contextInfo' => $contextInfo, // current module context info
             'enableWidgets' => array(// enable sub widgets
-                WacComponentList::$moduleToolBar,
-                WacComponentList::$moduleList,
-                WacComponentList::$moduleForm
+                WacComponentList::$moduleUploadForm
             )
         )
 );
-
 ?>
